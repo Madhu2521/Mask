ed7dc267ec5312fe433fc2438d2bf5de2a3d4bdc
Madhumitha <118741796+Madhu2521@users.noreply.github.com>
2024-10-08 11:27:09 +0530

    Create FileSearchResult.cs

---
+++ FileSearchResult.cs
@@ -0,0 +1,8 @@
+namespace TextFinderAndMaskingTool.Models
+{
+    public class FileSearchResult
+    {
+        public string FileName { get; set; }
+        public int Occurrences { get; set; }
+    }
+}
